516e1fff9ce10892fe1ebc8239fff3bcd301f5ac
Paul Newling <PaulNewling@Gmail.com>
2021-03-07 10:28:15 -0800

    Connected Parse

--- Instagram_Clone/AppDelegate.swift
+++ Instagram_Clone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "Mc6EV4sMv1Avq3msH1amkE1dMxI1lqrLH2heQbEP" // <- UPDATE
+                $0.clientKey = "YkNSVFQ4PB1BSjHJBkNTJ86bsFBooxUsoRtHa2sB" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
